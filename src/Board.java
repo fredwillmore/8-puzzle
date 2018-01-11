b45b530797c7d59ffd9f6f24c42d6cb24384bf05
Fred Willmore <fredwillmore@gmail.com>
2018-01-10 17:06:55 -0800

    code passes automated test with 99/100

---
+++ src/Board.java
@@ -0,0 +1,277 @@
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class Board {
+
+    private final int[][] blocks;
+//    private final Board goal;
+    private final int n;
+
+    public Board(int[][] blocks) {
+        this.blocks = blocks.clone();
+        n = blocks.length;
+
+    }
+
+    public int dimension() {                 // board dimension n
+        return n;
+    }
+
+    public int hamming() {                   // number of blocks out of place
+        int score = 0;
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                score += hammingDistance(x, y, blocks[y][x]);
+//                score += (blocks[y][x] != 0 && (((blocks[y][x] - 1) % n) != x || ((blocks[y][x] - 1) / n) != y)) ? 1 : 0;
+            }
+        }
+        return score;
+    }
+
+    private int hammingDistance(int x, int y, int v) {
+        if (v != 0 && (goalCol(v) != x || goalRow(v) != y))
+            return 1;
+        else
+            return 0;
+    }
+
+    public int manhattan() {                 // sum of Manhattan distances between blocks and goal
+        int score = 0;
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                score += manhattanDistance(x, y, blocks[y][x]);
+//                score += (blocks[y][x] == 0 ? 0 : Math.abs(((blocks[y][x] - 1) % n) - x) + Math.abs(((blocks[y][x] - 1) / n) - y));
+            }
+        }
+        return score;
+    }
+
+    private int manhattanDistance(int x, int y, int v) {
+        return v == 0 ? 0 : Math.abs(goalCol(v) - x) + Math.abs(goalRow(v) - y);
+    }
+
+    private int goalRow(int value) {
+        return (value - 1) / n;
+    }
+
+    private int goalCol(int value) {
+        return (value - 1) % n;
+    }
+
+    public boolean isGoal() {                // is this board the goal board?
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (blocks[i][j] != (i*n + j + 1) % (n*n)) return false;
+            }
+        }
+        return true;
+    }
+
+    public Board twin() {                    // a board that is obtained by exchanging any pair of blocks
+        return new Board(twinBlocks());
+    }
+
+    private int[][] twinBlocks() {
+        int[][] b = cloneBlocks();
+        int x1 = 0, y1 = 0, x2 = 1, y2 = 0;
+
+        if (b[y1][x1] == 0 || b[y2][x2] == 0) {
+            y1 = 1;
+            y2 = 1;
+        }
+        swapBlocks(b, x1, y1, x2, y2);
+        return b;
+    }
+
+    private void swapBlocks(int[][] b, int x1, int y1, int x2, int y2) {
+        int temp = b[y1][x1];
+        b[y1][x1] = b[y2][x2];
+        b[y2][x2] = temp;
+    }
+
+    private int[][] cloneBlocks() {
+        int[][] b = new int[n][0];
+        for (int i = 0; i < n; i++)
+            b[i] = blocks[i].clone();
+        return b;
+    }
+
+
+    public boolean equals(Object y) {        // does this board equal y?
+        return y == null ? false : this.toString().equals(y.toString());
+        // TODO: I don't want to depend on the string representation, but the API for Board doesn't give me access to blocks
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if(blocks[i][j] != y.blocks) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+    }
+
+    public Iterable<Board> neighbors() {     // all neighboring boards
+        ArrayList<Board> it = new ArrayList<Board>();
+        int zeroRow = 0, zeroCol = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (blocks[i][j] == 0) {
+                    zeroRow = i;
+                    zeroCol = j;
+                    break;
+                }
+            }
+        }
+        int[][] neighborBlocks;
+        if (zeroCol > 0) {
+            neighborBlocks = cloneBlocks();
+            swapBlocks(neighborBlocks, zeroCol-1, zeroRow, zeroCol, zeroRow);
+            it.add(new Board(neighborBlocks));
+        }
+        if (zeroCol < n-1) {
+            neighborBlocks = cloneBlocks();
+            swapBlocks(neighborBlocks, zeroCol+1, zeroRow, zeroCol, zeroRow);
+            it.add(new Board(neighborBlocks));
+        }
+        if (zeroRow > 0) {
+            neighborBlocks = cloneBlocks();
+            swapBlocks(neighborBlocks, zeroCol, zeroRow-1, zeroCol, zeroRow);
+            it.add(new Board(neighborBlocks));
+        }
+        if (zeroRow < n-1) {
+            neighborBlocks = cloneBlocks();
+            swapBlocks(neighborBlocks, zeroCol, zeroRow+1, zeroCol, zeroRow);
+            it.add(new Board(neighborBlocks));
+        }
+
+        return it;
+    }
+
+    public String toString() {               // string representation of this board (in the output format specified below)
+        int fieldWidth = (int) Math.ceil(Math.log10(n*n)) + 1;
+//        int fieldWidth = Math.ceil(Math.log(n*n)) + 2;
+        String str = "" + n + "\n";
+        for (int[] row: blocks) {
+            for (int value: row) {
+                String display = Integer.toString(value);
+                str += String.format("%" + fieldWidth + "s", display);
+            }
+            str += "\n";
+        }
+//        System.out.println(str);
+        return str;
+    }
+
+    public static void main(String[] args) { // unit tests (not graded)
+//        testBoard();
+//        testEquals();
+//        testTwin();
+//        testIsGoal();
+//        testHamming();
+//        testManhattan();
+//        testNeighbors();
+//        testToString();
+        testImmutable();
+    }
+
+    private static final int[][] testBlocks0 = {{1, 2}, {0, 3}};
+    private static final int[][] testBlocks1 = {{4, 5, 6}, {0, 2, 3}, {7, 8, 1}};
+    private static final int[][] testBlocks2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+
+    private static final int[][] testBlocks3 = {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}};
+    private static final int[][] testBlocks4 = {{1,  3}, {0,  2}};
+    private static final int[][] TEST_TO_STRING = {
+            {2,  3,  4,  8},
+            {1,  6,  0, 12},
+            {5, 10,  7, 11},
+            {9, 13, 14, 15}
+    };
+    private static final int[][] TEST_TWIN = {
+            { 0,  1,  2,  3},
+            { 5,  6,  7,  4},
+            { 9, 10, 11,  8},
+            { 13, 14, 15, 12 }
+    };
+
+    private static final int[][] TEST_IMMUTABLE = {
+            { 1,  4,  3 },
+            { 7,  0,  8 },
+            { 6,  5,  2 }
+    };
+
+    private static void testBoard() {           // construct a board from an n-by-n array of blocks
+                                                // (where blocks[i][j] = block in row i, column j)
+        Board board = new Board(testBlocks1);
+    }
+
+    private static void testEquals() {        // does this board equal y?
+        Board board1 = new Board(testBlocks1);
+        Board board2 = new Board(testBlocks1.clone());
+        Board board3 = new Board(testBlocks3);
+        String description = "board1 equals board2";
+        String p = board1.equals(board2) ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testTwin() {                    // a board that is obtained by exchanging any pair of blocks
+        Board board1 = new Board(TEST_TWIN);
+        Board board2 = board1.twin();
+        String description = "board2 is twin of board1";
+        String p = board1.equals(board2) ? "failed" : "passed";
+        System.out.println(p + ": " + description);
+        System.out.println("board1: " + board1.toString());
+        System.out.println("board2: " + board2.toString());
+    }
+
+    private static void testIsGoal() {
+        Board board1 = new Board(testBlocks1);
+        Board board2 = new Board(testBlocks2);
+        String description = "board1 is not goal";
+        String p = board1.isGoal() ? "failed" : "passed";
+        System.out.println(p + ": " + description);
+        description = "board2 is goal";
+        p = board2.isGoal() ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testHamming() {
+        Board board3 = new Board(testBlocks3);
+        String description = "hamming";
+        String p = board3.hamming() == 5 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testNeighbors() {
+        Board board3 = new Board(testBlocks3);
+        System.out.println(board3);
+        System.out.println(board3.neighbors());
+        String description = "neighbors";
+        String p = board3.hamming() == 5 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+
+    }
+
+    private static void testManhattan() {
+        Board board = new Board(testBlocks4);
+        System.out.println(board.manhattan());
+        String description = "manhattan";
+        String p = board.manhattan() == 3 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+
+    }
+
+    private static void testToString() {
+        Board board = new Board(TEST_TO_STRING);
+        System.out.println(board);
+    }
+
+    private static void testImmutable() {
+        Board board = new Board(TEST_IMMUTABLE);
+        System.out.println(board.manhattan());
+        board.swapBlocks(TEST_IMMUTABLE, 0,0,1,0);
+        System.out.println(board.manhattan());
+    }
+
+
+}
---
+++ src/Solver.java
@@ -0,0 +1,211 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.lang.IllegalArgumentException;
+import edu.princeton.cs.algs4.MinPQ;
+
+public class Solver {
+
+//    public int debugCount = 0;
+    private class Node {
+        private final Node predecessor;
+        private final Board board;
+        private final int moves;
+        private final int score;
+
+        public Node(Board board, int moves, Node predecessor) {
+            this.board = board;
+            this.moves = moves;
+            this.predecessor = predecessor;
+            this.score = board.manhattan() + moves;
+//            System.out.println("this should be getting bigger: " + moves);
+
+        }
+
+        public Board getBoard() {
+            return board;
+        }
+
+        public int getMoves() {
+            return moves;
+        }
+    }
+
+    private int moves;
+    private boolean solvable;
+    private Node last = null;
+
+    public Solver(Board initial) {
+        if(initial == null)
+            throw new IllegalArgumentException();
+
+        Comparator<Node> thinger = (Node a, Node b) -> {
+            return Integer.compare(a.score, b.score);
+        };
+
+        MinPQ<Node> pq = new MinPQ<Node>(1, thinger);
+        pq.insert(new Node(initial, 0, null));
+
+        MinPQ<Node> pqAlt = new MinPQ<Node>(1, thinger);
+        pqAlt.insert(new Node(initial.twin(), 0, null));
+
+        Node test;
+        boolean found, foundAlt;
+        do {
+            found = step(pq);
+            foundAlt = step(pqAlt);
+        } while (!(found || foundAlt));
+
+        moves = found ? pq.min().getMoves() : -1;
+        solvable = found;
+    }           // find a solution to the initial board (using the A* algorithm)
+
+    private boolean step(MinPQ<Node> pq) {
+        boolean found = false;
+        Node test = pq.min();
+        if (test != null && test.getBoard().isGoal()) {
+            last = test;
+            return true;
+        }
+        test = pq.delMin();
+        for (Board neighbor: test.getBoard().neighbors()) {
+            if (null == test.predecessor || !neighbor.equals(test.predecessor.board)) {
+                pq.insert(new Node(neighbor, test.moves + 1, test));
+            }
+        }
+        return false;
+    }
+
+    public boolean isSolvable() {
+        return solvable;
+    }
+
+    public int moves() {
+        return moves;
+    }                     // min number of moves to solve initial board; -1 if unsolvable
+
+    public Iterable<Board> solution() {
+        if(!solvable)
+            return null;
+
+        ArrayList<Board> it = new ArrayList<Board>();
+        Node tracer = last;
+        do {
+            it.add(0, tracer.board);
+            tracer = tracer.predecessor;
+        } while (tracer != null);
+
+        return it;
+    }      // sequence of boards in a shortest solution; null if unsolvable
+
+    public static void main(String[] args) {
+//        testAlreadySolved();
+//        testOneMoveSimple();
+//        testUnsolvableSimple();
+//        testBiggerSolvable();
+//        testBiggerUnsolvable();
+//        testMoves();
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+
+    } // solve a slider puzzle (given below)
+
+    private static final int[][] TESTBLOCKS0 = {{1, 2}, {3, 0}};
+    private static final int[][] TESTBLOCKS1 = {{1, 2}, {0, 3}};
+    private static final int[][] TESTBLOCKS2 = {{1, 3}, {0, 2}};
+    private static final int[][] TESTBLOCKS3 = {
+            {1, 2, 3},
+            {7, 0, 4},
+            {8, 6, 5}
+    };
+    private static final int[][] TESTBLOCKS4 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {8, 7, 0}
+    };
+    private static final int[][] TEST_MOVES = {
+            {2,  3,  4,  8},
+            {1,  6,  0, 12},
+            {5, 10,  7, 11},
+            {9, 13, 14, 15}
+    };
+
+    private static void testAlreadySolved() {
+        Solver s = new Solver(new Board(TESTBLOCKS0));
+
+        System.out.println(s.moves);
+        String description = "board already solved";
+        String p = s.moves == 0 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testOneMoveSimple() {
+        Solver s = new Solver(new Board(TESTBLOCKS1));
+
+        String description = "simple board one move";
+        String p = s.moves == 1 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testUnsolvableSimple() {
+        Solver s = new Solver(new Board(TESTBLOCKS2));
+
+        String description = "simple board no solution";
+        String p = s.moves == -1 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testBiggerSolvable() {
+        Solver s = new Solver(new Board(TESTBLOCKS3));
+
+        String description = "bigger board solvable";
+        String p = s.moves == 8 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testBiggerUnsolvable() {
+        Solver s = new Solver(new Board(TESTBLOCKS4));
+
+        System.out.println(s.moves);
+        String description = "bigger board unsolvable";
+        String p = s.moves == -1 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+    private static void testMoves() {
+        Solver s = new Solver(new Board(TEST_MOVES));
+
+        System.out.println(s.moves);
+        String description = "moves";
+        String p = s.moves == 13 ? "passed" : "failed";
+        System.out.println(p + ": " + description);
+    }
+
+//    private static void testImmutable() {
+//        Board board = new Board(TEST_IMMUTABLE);
+//        System.out.println(board.manhattan());
+//        board.swapBlocks(TEST_IMMUTABLE, 0,0,1,0);
+//        System.out.println(board.manhattan());
+//    }
+
+}
